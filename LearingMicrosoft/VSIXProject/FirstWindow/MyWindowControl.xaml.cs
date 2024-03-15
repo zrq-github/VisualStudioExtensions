97e858510ceca7521a7032a812ea3328f331f5d1
zrq <wyzrq163@163.com>
2024-03-15 16:03:02 +0800

    FirstMenuCommand FirstWindow

---
+++ LearingMicrosoft/VSIXProject/FirstWindow/MyWindowControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FirstWindow
+{
+    /// <summary>
+    /// Interaction logic for MyWindowControl.
+    /// </summary>
+    public partial class MyWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyWindowControl"/> class.
+        /// </summary>
+        public MyWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "MyWindow");
+        }
+    }
+}
