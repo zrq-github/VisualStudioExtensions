cefb0b4bf31b0b683d63fff39abd280d81f0e35f
zrq <wyzrq163@163.com>
2024-03-15 19:15:46 +0800

    InsertGuid

---
+++ LearingMicrosoft/VSIXProject/InsertGuid/Commands/MyCommand.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Text;
+using System.Linq;
+
+namespace InsertGuid
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+            if (!selection.HasValue)
+                return;
+
+            var insertGuid = Guid.NewGuid().ToString();
+            docView.TextBuffer.Replace(selection.Value, insertGuid);
+        }
+    }
+}
